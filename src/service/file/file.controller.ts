0487d2bd6514f2cd5b5e101b6790b19987e112fb
jweboy <jweboy0630@gmail.com>
2024-12-11 23:31:34 +0800

    feat: 新增二维码配置

--- src/service/file/file.controller.ts
+++ src/service/file/file.controller.ts
@@ -6,9 +6,13 @@ import * as QRCode from 'qrcode';
 @ApiTags('File')
 @Controller('file')
 export class FileController {
-  @Post('canvas')
+  @Post('qrcode')
   @HttpCode(HttpStatus.OK)
   generateCanvas(@Body() dto: CreateCanvasDto) {
-    return QRCode.toDataURL(dto.text);
+    return QRCode.toDataURL(dto.text, {
+      errorCorrectionLevel: 'H', // 错误纠正级别：L, M, Q, H（H 为最高级别）
+      scale: 10, // 缩放倍数，值越大，颗粒度越密
+      margin: 2, // 二维码边距
+    });
   }
 }
